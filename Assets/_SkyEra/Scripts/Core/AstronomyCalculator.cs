eeddfc16a26ed1770bc27a46bb840e796185b1cc
Himsara99 <luthira2002@gmail.com>
2026-09-15 21:47:12 +0530

    9_15

--- Assets/_SkyEra/Scripts/Core/AstronomyCalculator.cs
+++ Assets/_SkyEra/Scripts/Core/AstronomyCalculator.cs
@@ -2,39 +2,72 @@ using UnityEngine;
 
 public class AstronomyCalculator : MonoBehaviour
 {
-    [Header("Observer Location - Sri Lanka")]
-    public float latitude = 6.9271f;
-    public float longitude = 79.8612f;
+
+    // Observer location (Sri Lanka)
+    public float latitude = 7.8731f;
+    public float longitude = 80.7718f;
 
 
-    // Calculate Local Sidereal Time
-    public float CalculateSiderealTime()
-    {
-        // Temporary value
-        // Full astronomical time calculation will be added next
-
-        return 0f;
-    }
-
-
-    // Convert Right Ascension + Declination
-    // into Altitude + Azimuth
-
+    // Calculate Altitude and Azimuth
     public Vector2 CalculateAltAz(
         float rightAscension,
         float declination
     )
     {
-        float altitude = 0f;
-        float azimuth = 0f;
+
+        // Temporary Local Sidereal Time
+        float lst = rightAscension + 6.0f;
 
 
-        // Calculation will be added here
+        // Hour angle
+        float hourAngle = lst - rightAscension;
+
+
+        // Convert degrees
+        float haRad = hourAngle * Mathf.Deg2Rad;
+        float decRad = declination * Mathf.Deg2Rad;
+        float latRad = latitude * Mathf.Deg2Rad;
+
+
+
+        // Altitude calculation
+
+        float altitude =
+            Mathf.Asin(
+                Mathf.Sin(decRad) * Mathf.Sin(latRad)
+                +
+                Mathf.Cos(decRad) * Mathf.Cos(latRad) * Mathf.Cos(haRad)
+            );
+
+
+        altitude *= Mathf.Rad2Deg;
+
+
+
+        // Azimuth calculation
+
+        float azimuth =
+            Mathf.Atan2(
+                -Mathf.Sin(haRad),
+                Mathf.Tan(decRad) * Mathf.Cos(latRad)
+                -
+                Mathf.Sin(latRad) * Mathf.Cos(haRad)
+            );
+
+
+        azimuth *= Mathf.Rad2Deg;
+
+
+        if (azimuth < 0)
+            azimuth += 360;
+
 
 
         return new Vector2(
             altitude,
             azimuth
         );
+
     }
+
 }
